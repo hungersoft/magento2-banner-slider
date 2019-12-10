94027c2083fa0e8936bc785b818b9140f9d05fc1
ES <eon@earncef.com>
2019-12-10 18:11:47 +0530

    Added helper methods

--- Helper/Data.php
+++ Helper/Data.php
@@ -17,21 +17,68 @@
 
 namespace HS\BannerSlider\Helper;
 
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\App\Helper\AbstractHelper;
 
 class Data extends AbstractHelper
 {
+    const CONFIG_ENABLED = 'hs_banner_slider/general/enabled';
+
     /**
-     * @param \Magento\Framework\App\Helper\Context $context
+     * Currently selected store ID if applicable.
+     *
+     * @var int
      */
-    public function __construct(
-        \Magento\Framework\App\Helper\Context $context
-    ) {
+    protected $_storeId = null;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @param Context               $context
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(Context $context, StoreManagerInterface $storeManager)
+    {
+        $this->storeManager = $storeManager;
         parent::__construct($context);
     }
 
-    public function getDefaultConfigValue($field, $store = null)
+    /**
+     * Get config value by path.
+     *
+     * @param string $path
+     *
+     * @return mixed
+     */
+    public function getConfigValue($path, $storeId = null)
     {
-        return;
+        return $this->scopeConfig->getValue($path, ScopeInterface::SCOPE_STORE, $storeId);
+    }
+
+    /**
+     * Get config flag by path.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function getConfigFlag($path)
+    {
+        return $this->scopeConfig->isSetFlag($path, ScopeInterface::SCOPE_STORE, $this->_storeId);
+    }
+
+    /**
+     * Return true if active and false otherwise.
+     *
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return $this->getConfigFlag(self::CONFIG_ENABLED);
     }
 }
