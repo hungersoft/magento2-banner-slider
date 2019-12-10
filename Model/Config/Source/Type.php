cf3a0549693cdbaf533c429b50fde4b4c8279b64
ES <eon@earncef.com>
2019-12-10 11:32:15 +0530

    Added image type options (Only image available in lite version)

---
+++ Model/Config/Source/Type.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @category  HS
+ *
+ * @copyright Copyright (c) 2015 Hungersoft (http://www.hungersoft.com)
+ * @license   http://www.hungersoft.com/license.txt Hungersoft General License
+ */
+
+namespace HS\BannerSlider\Model\Config\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+class Type implements ArrayInterface
+{
+    const IMAGE = 'image';
+
+    public function toOptionArray()
+    {
+        $result = [];
+        foreach ($this->getOptions() as $value => $label) {
+            $result[] = [
+                 'value' => $value,
+                 'label' => $label,
+             ];
+        }
+
+        return $result;
+    }
+
+    public function getOptions()
+    {
+        return [
+            self::IMAGE => __('Image'),
+        ];
+    }
+}
