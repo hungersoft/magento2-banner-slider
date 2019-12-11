f700dc38569df3bd7a74b9c8f75576ec291e02c1
ES <eon@earncef.com>
2019-12-11 11:19:19 +0530

    Fixed minor issues and added slider banner relation save code (untested).

--- Controller/Adminhtml/Slider/Save.php
+++ Controller/Adminhtml/Slider/Save.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Copyright 2019 Hungersoft (http://www.hungersoft.com).
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -48,22 +48,29 @@ class Save extends \Magento\Backend\App\Action
         if ($data) {
             $id = $this->getRequest()->getParam('slider_id');
 
-            $model = $this->_objectManager->create(\HS\BannerSlider\Model\Slider::class)->load($id);
-            if (!$model->getId() && $id) {
+            $slider = $this->_objectManager->create(\HS\BannerSlider\Model\Slider::class)->load($id);
+            if (!$slider->getSliderId() && $id) {
                 $this->messageManager->addErrorMessage(__('This Slider no longer exists.'));
 
                 return $resultRedirect->setPath('*/*/');
             }
 
-            $model->setData($data);
+            $slider->setData($data);
+            if (isset($data['slider_banners'])
+                && is_string($data['slider_banners'])
+                && !$category->getBannersReadonly()
+            ) {
+                $banners = json_decode($data['slider_banners'], true);
+                $slider->setPostedBanners($banners);
+            }
 
             try {
-                $model->save();
+                $slider->save();
                 $this->messageManager->addSuccessMessage(__('You saved the Slider.'));
                 $this->dataPersistor->clear('hs_banner_slider_slider');
 
                 if ($this->getRequest()->getParam('back')) {
-                    return $resultRedirect->setPath('*/*/edit', ['slider_id' => $model->getId()]);
+                    return $resultRedirect->setPath('*/*/edit', ['slider_id' => $slider->getId()]);
                 }
 
                 return $resultRedirect->setPath('*/*/');
